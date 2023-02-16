c7a3f8687d00be852286790fc9db0f6e0243e879
Anton Vakhania <antonvakhania@mail.ru>
2023-02-16 12:07:40 +0300

    Project has completed

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing';
+import Business from './Business';
+import Button from './Button';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import Hero from './Hero';
+import Navbar from './Navbar';
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Stats,
+    Testimonials,
+    Hero,
+    Navbar
+}
